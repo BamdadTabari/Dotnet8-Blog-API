967fc10e11374ac22fc395a639a826e9e1bae135
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2024-11-01 09:46:56 -0700

    add nugets

---
+++ DataProvider/Base/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Base;
+public interface IBaseEntity
+{
+}
+public abstract class BaseEntity<T> : IBaseEntity
+{
+    public T Id { get; set; }
+
+    // last changes datetime
+    public DateTime ModifiedDate { get; set; }
+    public DateTime CreateDate { get; set; }
+    public bool IsDeleted { get; set; }
+}
+
+// this is default for id 
+//you see default id type is long
+public abstract class BaseEntity : BaseEntity<int>
+{
+}
---
+++ DataProvider/Base/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Base;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+
+    //define entities here
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasDefaultSchema("dbo");
+
+        //var assembly = typeof(IBaseEntity).Assembly;
+        ////dbset entities
+        //modelBuilder.RegisterAllEntities<IBaseEntity>(assembly);
+
+        ////config entities (configs like IEntityTypeConfiguration,.....)
+        //modelBuilder.RegisterEntityTypeConfiguration(assembly);
+
+    }
+}
